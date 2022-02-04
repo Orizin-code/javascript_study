79e49c1075cedf23d9e6802e456d41befe179ac2
Orizin <nlgporizin10@gmail.com>
2022-02-04 22:59:40 +0900

    関数で共通化/enterキーで投稿機能実装

--- post_js_sample/script.js
+++ post_js_sample/script.js
@@ -2,7 +2,15 @@ const messageField = document.getElementById('message-field')
 const button = document.getElementById('message-button')
 const messageList = document.getElementById('message-list')
 
-button.onclick = () => {
+const addMessage = () => {
   messageList.insertAdjacentHTML('afterbegin', `<p>${messageField.value}</p>`)
   messageField.value = ''
+}
+
+// ボタンクリックで投稿
+button.onclick = () => addMessage()
+
+// enterキーで投稿
+messageField.onkeydown = (e) => {
+  if (e.keyCode == '13') addMessage()
 }
